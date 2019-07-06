3caf144b94c938b785bbd8a5c9d56cec8b5fe2ff
leiyu <yuleics@outlook.com>
2019-07-06 15:33:20 +0800

    add

---
+++ HelloWorld/src/com/lesliefish/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.lesliefish;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+    private String message;
+
+    public void init() throws ServletException {
+        message = "hey. this is my first servlet web app.";
+        System.out.println("init message :" + message);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>" + message + "</h1>");
+    }
+}
